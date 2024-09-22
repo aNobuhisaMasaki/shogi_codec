f027c7b7d1e170c53c5a61c3bae1bdf807d3c56b
Nobuhisa Masaki <nobuhisa.masaki@access-company.com>
2024-09-23 04:59:35 +0900

    Add estimate script

---
+++ src/estimate.js
@@ -0,0 +1,31 @@
+const decode = require('./decode')
+
+const confidenceInterval = (successes, trials) => {
+  // 二項分布の推定確率
+  const p = successes / trials
+  const z = 1.96; // 95%の信頼区間
+
+  const standardError = Math.sqrt(p * (1 - p) / trials)
+
+  return {
+    min: Math.max(0, p - z * standardError),
+    max: Math.min(1, p + z * standardError),
+  }
+}
+
+const estimate = async (trials = 100) => {
+  const results = await [...Array(trials)].reduce((a, i) => a.then(async a => [...a, await decode()]), Promise.resolve([]))
+  const successes = results.filter(result => result.data.isValid).length
+  const allPatterns = Number(results[0].data.allBoardPatterns.value)
+  console.log(allPatterns)
+  console.log(successes)
+  const { min, max } = confidenceInterval(successes, trials)
+  const result = {
+    min: min * allPatterns,
+    max: max * allPatterns,
+  }
+  console.log(result)
+  return result
+}
+
+module.exports = estimate
